841ec952eddd4c38fc323f247b67e5494fd02b03
shepard153 <kamil.kosmider1@gmail.com>
2023-03-15 16:32:41 +0100

    Add datalist label and placeholder

--- packages/Emsit/BagistoInPostShipping/src/Resources/views/shop/dropdown.blade.php
+++ packages/Emsit/BagistoInPostShipping/src/Resources/views/shop/dropdown.blade.php
@@ -1,5 +1,10 @@
 <div id="paczkomaty_locations_dropdown" class="row col-12 form-field" style="display: none">
-    <input list="paczkomaty_locations" id="paczkomaty_locations_search" class="form-control"/>
+    <label for="paczkomaty_locations_search" class="form-label">Wybierz docelowy paczkomat</label>
+    <input list="paczkomaty_locations"
+           id="paczkomaty_locations_search"
+           class="form-control"
+           placeholder="{{ sprintf('[%s] %s %s %s', $lockers[0]->name, $lockers[0]->address, $lockers[0]->post_code, $lockers[0]->city) }}"
+           required/>
     <datalist id="paczkomaty_locations">
         @foreach ($lockers as $locker)
             <option
